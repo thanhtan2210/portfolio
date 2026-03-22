d09e1a2524b486d04474ba9fe8b29c8851e8afb6
thanhtan2210 <tanbanquyen@gmail.com>
2026-03-22 23:09:06 +0700

    docs: upload cv

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -50,7 +50,7 @@ const Header = () => {
                     initial={{ y: 30, opacity: 0 }}
                     whileInView={{ y: 0, opacity: 1 }}
                     transition={{ duration: 0.6, delay: 1 }}
-                    href="1.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>
+                    href="Phan Thanh Tan.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>
                     my resume <Image src={assets.download_icon} alt='' className='w-4' />
                 </motion.a>
             </div>
